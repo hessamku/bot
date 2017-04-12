4d1f5cfa4d184636b0bdee82522ee5a29199228a
hessamku <hesfahi@gmail.com>
2017-04-12 13:51:45 +0430

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = ""
+bot_token = "333561923:AAEqKb8VCBO-BNyPDVcLc4w1KiAMBhMDGgQ"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "2.5"
-sudo_id = 212400472
+sudo_id = 299304423
